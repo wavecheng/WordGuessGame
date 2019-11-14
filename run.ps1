05a89bce2ab6369a0f835ea4b18de63568bbd542
Bo Chen <bo.chen@citrix.com>
2019-11-14 13:57:41 +0800

    add chengyu xiehouyu with explanation dict

--- run.ps1
+++ run.ps1
@@ -1,3 +1,3 @@
-.\generate.ps1 -FilePath 小学语文二上.txt -OutputFile 小学语文二上.pptx
+.\generate.ps1 -FilePath 小学语文二上.txt -OutputFile 小学语文二上.pptx -Size 10
 
-.\generate.ps1 -FilePath 2000-sample.txt -OutputFile 2000sample.pptx
+.\generate.ps1 -FilePath 2000-sample.txt -OutputFile 2000sample.pptx -Size 10
